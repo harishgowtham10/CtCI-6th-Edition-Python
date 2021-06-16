6caaa32c4fa6f0dab202b146a4ff40ba43f7d31c
Harish Gowtham S <47297271+harishgowtham10@users.noreply.github.com>
2021-06-16 10:02:48 +0530

    Update p02_check_permutation.py

--- chapter_01/p02_check_permutation.py
+++ chapter_01/p02_check_permutation.py
@@ -25,7 +25,40 @@ def check_permutation_by_count(str1, str2):
         counter[ord(c)] -= 1
     return True
 
+#sliding window algorithm - TC: O(n+m)
+def find_permutation(str, pattern):
+      window_start, matched = 0, 0
+      char_frequency = {}
 
+      for chr in pattern:
+        if chr not in char_frequency:
+          char_frequency[chr] = 0
+        char_frequency[chr] += 1
+
+      # our goal is to match all the characters from the 'char_frequency' with the current window
+      # try to extend the range [window_start, window_end]
+      for window_end in range(len(str)):
+        right_char = str[window_end]
+        if right_char in char_frequency:
+          # decrement the frequency of matched character
+          char_frequency[right_char] -= 1
+          if char_frequency[right_char] == 0:
+            matched += 1
+
+        if matched == len(char_frequency):
+          return True
+
+        # shrink the window by one character
+        if window_end >= len(pattern) - 1:
+          left_char = str[window_start]
+          window_start += 1
+          if left_char in char_frequency:
+            if char_frequency[left_char] == 0:
+              matched -= 1
+            char_frequency[left_char] += 1
+
+      return False
+    
 def check_permutation_pythonic(str1, str2):
     if len(str1) != len(str2):
         return False
